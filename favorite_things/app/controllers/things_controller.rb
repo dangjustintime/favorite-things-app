4dbcd112b567fdafb57402371221c2f8cb32d47a
dangjustintime <dangjustintime@gmail.com>
2018-08-10 13:21:17 -0400

    added controllers for users and things

---
+++ favorite_things/app/controllers/things_controller.rb
@@ -0,0 +1,18 @@
+class ThingsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index
+        render json: Thing.all
+    end
+    def show
+        render json: Thing.find(params["id"])
+    end
+    def create
+        render json: Thing.create(params["thing"])
+    end
+    def delete
+        render json: Thing.delete(params["id"])
+    end
+    def update
+        render json: Thing.create(params["id"], params["thing"])
+    end
+end
