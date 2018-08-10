2b639e28ea2faec3f8fed731c057c88770879ea3
dangjustintime <dangjustintime@gmail.com>
2018-08-10 13:13:04 -0400

    added routes for users and things

--- favorite_things/config/routes.rb
+++ favorite_things/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # thing routes
+  get '/things', to: 'things#index'
+  get '/things/:id', to: 'things#show'
+  post '/things', to: 'things#create'
+  delete '/things/:id', to: 'users#delete'
+  put '/things/:id', to: 'users#update'
+
+  # user routes
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#delete'
+  put '/users/:id', to: 'users#update'
 end
