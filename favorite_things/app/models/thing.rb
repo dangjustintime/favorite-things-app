a1a8511e04bcd80c8964bea19829fd9bc37746f3
dangjustintime <dangjustintime@gmail.com>
2018-08-13 16:26:26 -0400

    tested and fixed routes for things model

--- favorite_things/app/models/thing.rb
+++ favorite_things/app/models/thing.rb
@@ -21,7 +21,8 @@ class Thing
           <<-SQL
               SELECT
                   things.*,
-                  users.username
+                  users.username,
+                  users.password
               FROM things
               LEFT JOIN users
                   ON things.user_id = users.id
@@ -29,19 +30,18 @@ class Thing
       )
       return results.map do |result|
             if result["user_id"]
-                user = User.new(
-                    {
-                        "id" => result["user_id"],
-                        "username" => result["username"]
-                    }
-                )
+                user = {
+                    "id" => result["user_id"],
+                    "username" => result["username"],
+                    "password" => result["password"]
+                }
             else
                 user = nil
             end
             Thing.new(
                 {
                     "id" => result["id"],
-                    "title" => result["name"],
+                    "title" => result["title"],
                     "image" => result["image"],
                     "user" => user,
                     "category" => result["category"],
@@ -57,7 +57,8 @@ class Thing
             <<-SQL
                 SELECT
                     things.*,
-                    users.username
+                    users.username,
+                    users.password
                 FROM things
                 LEFT JOIN users
                     ON things.user_id = users.id
@@ -66,59 +67,57 @@ class Thing
         )
         result = results.first
         if result["user_id"]
-            user = User.new(
+            user = 
                 {
                     "id" => result["user_id"],
-                    "username" => result["username"]
+                    "username" => result["username"],
+                    "password" => result["password"]
                 }
-            )
         else
             user = nil
         end
-        Thing.new(
-            {
-                "id" => result["id"],
-                "title" => result["name"],
+        return {
+                "id" => result["id"].to_i,
+                "title" => result["title"],
                 "image" => result["image"],
-                "user" => user,
                 "category" => result["category"],
-                "description" => result["description"]
-            }
-        )
-        return thing
+                "description" => result["description"],
+                "user" => user
+        }
     end
 
     # create one
-    def self.create(opts={})
+    def self.create(opts)
       results = DB.exec(
           <<-SQL
               INSERT INTO things (title, image, category, description, user_id)
               VALUES (
                 '#{opts["title"]}',
-                 #{opts["image"]},
+                 '#{opts["image"]}',
                  '#{opts["category"]}',
                  '#{opts["description"]}',
                 #{opts["user_id"] ? opts["user_id"] : "NULL"} )
               RETURNING id, title, image, category, description, user_id;
           SQL
       )
-      return Thing.new(results.first)
+      # return Thing.new(results.first)
     end
 
     # delete one (by id)
     def self.delete(id)
       results = DB.exec("DELETE FROM things WHERE id=#{id};")
-      return { deleted: true }
+
+      return { "deleted" => true }
     end
 
     # update one (by id)
-    def self.update(id, opts={})
+    def self.update(id, opts)
       results = DB.exec(
           <<-SQL
               UPDATE things
               SET
                title='#{opts["title"]}',
-               image=#{opts["image"]},
+               image='#{opts["image"]}',
                category='#{opts["category"]}',
                description='#{opts["description"]}',
                user_id=#{opts["user_id"] ? opts["user_id"] : "NULL"}
@@ -126,7 +125,14 @@ class Thing
               RETURNING id, title, image, category, description, user_id;
           SQL
       )
-      return Thing.new(results.first)
+      return {
+        "id" => results.first["id"].to_i,
+        "title" => results.first["title"],
+        "image" => results.first["image"],
+        "category" => results.first["category"],
+        "description" => results.first["description"],
+        "user_id" => results.first["user_id"].to_i
+      }
     end
 
     # update things
@@ -136,7 +142,7 @@ class Thing
             UPDATE things
             SET user_id = #{user.id}
             WHERE id = #{thing_id}
-            RETURNING id, username;
+            RETURNING id, username, password;
         SQL
     )
     return Thing.new(results.first)
--- favorite_things/config/routes.rb
+++ favorite_things/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get '/things', to: 'things#index'
   get '/things/:id', to: 'things#show'
   post '/things', to: 'things#create'
-  delete '/things/:id', to: 'users#delete'
-  put '/things/:id', to: 'users#update'
+  delete '/things/:id', to: 'things#delete'
+  put '/things/:id', to: 'things#update'
 
   # user routes
   get '/users', to: 'users#index'
