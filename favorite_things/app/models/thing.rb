6798e6603b1a0bcce845c394d29266fb4279f6b9
dangjustintime <dangjustintime@gmail.com>
2018-08-14 22:08:27 -0400

    added stuff to thing model to work with heroku

--- favorite_things/app/models/thing.rb
+++ favorite_things/app/models/thing.rb
@@ -1,7 +1,12 @@
 class Thing
     attr_reader :id, :title, :image, :category, :description
-
-    DB = PG.connect(host: "localhost", port: 5432, dbname: 'favorite_things_development')
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'favorite_things_development')
+    end
+    # DB = PG.connect(host: "localhost", port: 5432, dbname: 'favorite_things_development')
 
     # initialize options hash
     def initialize(opts = {})
