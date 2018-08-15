1f6f38fe886707bf7cf71654d7e31a8510d44d16
dangjustintime <dangjustintime@gmail.com>
2018-08-14 22:57:11 -0400

    added heroku connect to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User
-  DB = PG.connect(
-    host: "localhost",
-    port: 5432,
-    dbname: 'favorite_things_development'
-  )
+  if(ENV['DATABASE_URL'])
+      uri = URI.parse(ENV['DATABASE_URL'])
+      DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+      DB = PG.connect(host: "localhost", port: 5432, dbname: 'favorite_things_development')
+  end
   def self.all
     results = DB.exec("SELECT * FROM users");
     return results.map do |result|
